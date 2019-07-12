fdb91d459f9aa4746edfc1cab493335b1d7244ad
Matias Garcia <matiasigarcia91@gmail.com>
2019-07-12 16:45:42 +0200

    Table header

--- HEP/src/components/Table.tsx
+++ HEP/src/components/Table.tsx
@@ -12,6 +12,16 @@ export interface RowAttributes {
     checkbox: SectionDataElement;
 }
 
+const Header = () => (
+    <div class="header">
+        <div class="column-big" />
+        <div class="column-small antigen-tag">HBV</div>
+        <div class="column-small">SOURCE OF DATA</div>
+        <div class="column-small antigen-tag">HCV</div>
+        <div class="column-small">SOURCE OF DATA</div>
+    </div>
+);
+
 function Row(attributes: RowAttributes): string {
     const { field, checkbox } = attributes;
     return (
@@ -37,10 +47,10 @@ function Row(attributes: RowAttributes): string {
 export function Table(attributes: TableAttributes): string {
     const { fields, checkboxes } = attributes.section.formFields;
     return (
-        <div>
-            <div> HEADER </div>
+        <div class="table">
+            <Header />
+            <div class="elements"> Elements </div>
             <Row field={fields[0]} checkbox={checkboxes[0]} />
         </div>
     );
-    return "";
 }
