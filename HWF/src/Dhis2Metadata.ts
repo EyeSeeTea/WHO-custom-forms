5cebd3f0cafbc83b310d955d0ff65f6a83ceb39a
Matias Garcia <matiasigarcia91@gmail.com>
2019-07-20 00:59:38 +0200

    Setup infrastructure for programs

---
+++ HWF/src/Dhis2Metadata.ts
@@ -0,0 +1,100 @@
+import fetch from "node-fetch";
+import * as qs from "qs";
+
+import { safeParseJSON } from "./utils";
+//import { Section, DataEntryForm } from "./models/Form";
+
+export interface Ref {
+    id: string;
+}
+
+export interface DataEntryForm {
+    id: string;
+    name: string;
+    htmlCode: string;
+    style: "NORMAL" | "COMFORTABLE" | "COMPACT" | "NONE";
+}
+
+export interface Program {
+    id: string;
+    programStages: ProgramStage[];
+}
+
+export interface ProgramStage {
+    id: string;
+    formType: string;
+    dataEntryForm: Ref;
+    programStageDataElements: {
+        id: string;
+        dataElements: Ref[];
+    };
+}
+
+export interface MetadataPayload {
+    programStages: ProgramStage[];
+    dataEntryForms: DataEntryForm[];
+}
+
+export interface MetadataResponse {
+    importParams: object;
+    status: "OK" | "ERROR";
+    stats: object;
+    typeReports: object[];
+}
+
+export type MetadataParams = Partial<{
+    "programs:fields": string;
+    "programs:filter": string;
+}>;
+
+export type MetadataOptions = Partial<{
+    importStrategy: "CREATE_AND_UPDATE" | "CREATE" | "UPDATE" | "DELETE";
+}>;
+
+export type Dhis2MetadataOptions = {
+    debug: boolean;
+};
+
+export class Dhis2Metadata {
+    private apiUrl: string;
+
+    headers = { "Content-Type": "application/json" };
+
+    constructor(public baseUrl: string, public options: Dhis2MetadataOptions) {
+        this.apiUrl = baseUrl.replace(/\/*$/, "") + "/api";
+    }
+
+    private debug(message: string): void {
+        if (this.options.debug) {
+            console.debug(message);
+        }
+    }
+
+    private getEndpoint(path: string, params: object): string {
+        const queryStrings = qs.stringify(params, { indices: false });
+        const suffix = queryStrings ? `?${queryStrings}` : "";
+        const cleanPath = path.replace(/^\/*/, "");
+        return `${this.apiUrl}/${cleanPath}` + suffix;
+    }
+
+    async get<T>(params: MetadataParams): Promise<T> {
+        const url = this.getEndpoint("/metadata", params);
+        this.debug(`GET ${decodeURIComponent(url)}`);
+
+        return fetch(url, {
+            method: "GET",
+            headers: this.headers,
+        }).then(safeParseJSON);
+    }
+
+    async post(payload: MetadataPayload, options: MetadataOptions): Promise<MetadataResponse> {
+        const url = this.getEndpoint("/metadata", options);
+        this.debug(`POST ${url}`);
+
+        return fetch(url, {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: this.headers,
+        }).then(safeParseJSON);
+    }
+}
