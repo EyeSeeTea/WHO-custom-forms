f864aca996bd558dbf4b623c51984b26ae81808d
Alexis Rico <sferadev@gmail.com>
2019-08-13 14:15:21 +0000

    Add complete/undo button

--- HEP-data-entry/src/components/DhisSubmitButton.tsx
+++ HEP-data-entry/src/components/DhisSubmitButton.tsx
@@ -1,28 +1,57 @@
 import { createElement } from "typed-html";
 
+/**
+ * Submit buttons
+ * 
+ * NOTE: Client has removed the "complete" feature for data sets.
+ * To re-enable it, create a "completeButton" that executes "registerCompleteDataSet()".
+ */
 export function DhisSubmitButton() {
     return (
-        <div
-            id="completenessDivCustom"
-            class="page inputCriteria"
-            style="display: block; margin-top: 15px; width: 430px"
-        >
-            <input
-                type="button"
-                id="completeButton"
-                name="completeButton"
-                value="Submit: Check data consistency"
-                onclick="dhis2.de.validate( false ); registerCompleteDataSet()"
-                style="width:225px; margin-right:5px"
-            />
-            <input
-                type="button"
-                id="cancelButton"
-                name="cancelButton"
-                value="Go back to home page"
-                onclick="history.back()"
-                style="width:175px; margin-left:5px"
-            />
+        <div>
+            <div
+                id="validationDivCustom"
+                class="page inputCriteria"
+                style="display: flex; margin-top: 15px; width: 430px; padding:15px"
+            >
+                <input
+                    type="button"
+                    id="validateButton"
+                    value="Submit: Check data consistency"
+                    onclick="dhis2.de.validate(false)"
+                    style="margin-left:5px; margin-right:5px; flex-grow:2"
+                />
+                <input
+                    type="button"
+                    id="cancelButton"
+                    name="cancelButton"
+                    value="Go back to home page"
+                    onclick="history.back()"
+                    style="margin-left:5px; margin-right:5px; flex-grow:1"
+                />
+                </div>
+            <div
+                id="completenessDivCustom"
+                class="page inputCriteria"
+                style="display: flex; margin-top: 15px; width: 430px; padding:15px"
+            >
+                <input
+                    type="button"
+                    id="completeButton"
+                    name="completeButton"
+                    value="Complete"
+                    onclick="registerCompleteDataSet()"
+                    style="margin-left:5px; margin-right:5px; flex-grow:1"
+                />
+                <input
+                    type="button"
+                    id="undoButton"
+                    name="undoButton"
+                    value="Incomplete"
+                    onclick="undoCompleteDataSet()"
+                    style="margin-left:5px; margin-right:5px; flex-grow:1"
+                />
+            </div>
         </div>
     );
 }
