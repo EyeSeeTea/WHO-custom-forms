43e469feaf478f732142d56b03552260e95d4c96
Matias Garcia <matiasigarcia91@gmail.com>
2019-10-29 17:50:57 -0300

    Extend script to include tab (section) titles

--- HEP-data-entry/src/resources/custom-form.js
+++ HEP-data-entry/src/resources/custom-form.js
@@ -1,6 +1,10 @@
 async function applyChangesToForm() {
-    const { dataElementTranslations, userLocale } = await getLocaleAndTranslations();
-    await replaceDataElementTitles(dataElementTranslations, userLocale);
+    const {
+        dataElementTranslations,
+        sectionTranslations,
+        userLocale,
+    } = await getLocaleAndTranslations();
+    await replaceLocalizedTexts(dataElementTranslations, sectionTranslations, userLocale);
     const datasetToApplyChanges = $("#custom-form-script").attr("data-dataset-id");
     const currentDataset = $("#selectedDataSetId").val();
     $("#completenessDiv").wrap("<div id='completenessWrapper'></div>");
@@ -19,21 +23,37 @@ async function getLocaleAndTranslations() {
         const parsed = JSON.parse(body);
         return parsed.keyUiLocale;
     });
-    const dataElementTranslations = await fetch(
-        "http://localhost:8080/api/dataSets.json?fields=sections[dataElements[id,formName,translations]]&filter=id:eq:jfawDJZ5fOX",
+    const { dataElementTranslations, sectionTranslations } = await fetch(
+        "http://localhost:8080/api/dataSets.json?fields=sections[id,translations,dataElements[id,formName,translations]]&filter=id:eq:jfawDJZ5fOX",
         { method: "GET" }
     ).then(async response => {
         const body = await response.text();
         const parsed = JSON.parse(body);
-        const all = parsed.dataSets[0].sections.reduce((acc, s) => {
+        const allDataElements = parsed.dataSets[0].sections.reduce((acc, s) => {
             return [...acc, s.dataElements];
         }, []);
-        return _.flatten(all);
+
+        const allSections = parsed.dataSets[0].sections.reduce((acc, s) => {
+            return [...acc, { id: s.id, translations: s.translations }];
+        }, []);
+
+        return {
+            dataElementTranslations: _.flatten(allDataElements),
+            sectionTranslations: _.flatten(allSections),
+        };
     });
-    return { dataElementTranslations, userLocale };
+    return { dataElementTranslations, sectionTranslations, userLocale };
 }
 
-function replaceDataElementTitles(dataElementTranslations, userLocale) {
+function replaceLocalizedTexts(dataElementTranslations, sectionTranslations, userLocale) {
+    sectionTranslations.forEach(s => {
+        const translation = s.translations.find(
+            t => t.locale === userLocale && t.property === "NAME"
+        );
+        if (translation) {
+            $(`#${s.id}-section-title`).html(translation.value);
+        }
+    });
     dataElementTranslations.forEach(de => {
         const translation = de.translations.find(
             t => t.locale === userLocale && t.property === "FORM_NAME"
