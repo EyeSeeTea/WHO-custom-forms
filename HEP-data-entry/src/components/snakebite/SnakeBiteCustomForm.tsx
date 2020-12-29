d82f5dd258fdab839d79a706845f527fadd83a18
Jorge Sánchez <xurxosf@gmail.com>
2020-12-29 13:21:11 +0100

    Show tabs if subnational version tab is required

--- HEP-data-entry/src/components/snakebite/SnakeBiteCustomForm.tsx
+++ HEP-data-entry/src/components/snakebite/SnakeBiteCustomForm.tsx
@@ -10,18 +10,38 @@ export function SnakeBiteCustomForm(dataSet: DataSet, customMetadata: CustomMeta
                             });
                         </script>`;
 
+    const showSubnational = true;
+
     return (
         <div>
+            {javascript}
             <h2>
                 SNAKE BITE ENVENOMING
                 <br />
                 &nbsp;ANNUAL REPORT
             </h2>
-            {javascript}
 
-            <div id="tabs">
-                <Sections sections={dataSet.sections} customMetadata={customMetadata} />
-            </div>
+            {showSubnational ? (
+                <div id="tabs">
+                    <ul>
+                        <li>
+                            <a href="#tab0">National</a>
+                        </li>
+                        <li>
+                            <a href="#tab1">Subnational</a>
+                        </li>
+                    </ul>
+
+                    <div id="tab0">
+                        <Sections sections={dataSet.sections} customMetadata={customMetadata} />
+                    </div>
+                    <div id="tab1">{"Subnational"}</div>
+                </div>
+            ) : (
+                <div id="tabs">
+                    <Sections sections={dataSet.sections} customMetadata={customMetadata} />
+                </div>
+            )}
         </div>
     );
 }
